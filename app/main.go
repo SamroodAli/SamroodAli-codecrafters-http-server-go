c1cfbaacaa5b7c9423972bdb457d6bcdf1ca63d3
Samrood Ali <devsamrood@gmail.com>
2025-05-02 15:56:32 +0530

    feat: listen to connections on loop and write 200 response

--- app/main.go
+++ app/main.go
@@ -7,17 +7,26 @@ import (
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listen, err := net.Listen("tcp", "0.0.0.0:4221")
 
 	if err != nil {
 		fmt.Println("Error accepting connection", err.Error())
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	for {
+		connection, err := listen.Accept()
 
-	if err != nil {
-		fmt.Println("Error accepting connection", err.Error())
-		os.Exit(1)
+		if err != nil {
+			fmt.Println("Error accepting connection", err.Error())
+		}
+
+		go handleConnection(connection)
 	}
+}
+
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
+
+	connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
